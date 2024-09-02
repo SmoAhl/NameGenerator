9eb6f2cfc969d27d3c6913817c9c678740429308
Simo Ahlqvist <p19497@edu.sasky.fi>
2024-09-02 10:54:22 +0300

    first

---
+++ Pages/NameGenerator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NameGenerator.Pages
+{
+    public class NameGeneratorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
