53fbc0a42a664f863d7c06c066f13253da558465
Simo Ahlqvist <p19497@edu.sasky.fi>
2024-09-04 11:19:05 +0300

    name logic

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7839ffb07ba78e802f03e53b99dc605054b2b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59d353388cd02c1d68782041522994578838ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
