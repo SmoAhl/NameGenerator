d59d353388cd02c1d68782041522994578838ded
Simo Ahlqvist <p19497@edu.sasky.fi>
2024-09-04 10:26:57 +0300

    gandalf

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0768690e7da9d14d0a8fc2ff691b1494de3c56d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7839ffb07ba78e802f03e53b99dc605054b2b82")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
